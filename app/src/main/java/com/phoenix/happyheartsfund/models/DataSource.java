c854e029fc7e099795e8a103346a9bca3e53d1ed
prachi44 <prachikewalramani44@gmail.com>
2015-07-12 03:02:49 +0530

    cleaning repository

--- app/src/main/java/com/phoenix/happyheartsfund/models/DataSource.java
+++
@@ -1,9 +0,0 @@
-package com.phoenix.happyheartsfund.models;
-
-import java.util.ArrayList;
-
-public class DataSource {
-    public static ArrayList<Account> userAccounts;
-    public static ArrayList<MessagePost> experiencePosts;
-    public static ArrayList<ImagePost> photoPosts;
-}
